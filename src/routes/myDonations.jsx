b95a2b4548cba793a1d7ec80c9a4bb46a3515b5a
Jahirul Islam <j.islam.trine@gmail.com>
2023-10-03 02:12:05 +0600

    Fix:Added conditional rendering for donations.

--- src/routes/myDonations.jsx
+++ src/routes/myDonations.jsx
@@ -12,11 +12,22 @@ export async function donationDataLoader()
 export default function Donations()
 {
     const {data} = useLoaderData();
+    let content;
+    if(data)
+    {
+       content = <div className="grid md:grid-cols-4 gap-4">
+        {
+        data.map((card, index) => <Card key={index} data={card} /> )
+        }
+        </div>
+    }
+    else
+    {
+        content = <h3>No Donation So far</h3>
+    }
     return(
-        <div className="grid md:grid-cols-4 gap-4">
-                {
-                data.map((card, index) => <Card key={index} data={card} /> )
-                }
+        <div>
+            {content}
         </div>
     )
 }
